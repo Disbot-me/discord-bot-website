b73d1cb7ad0d01e96daaebf52b5f50c02c40b229
novadiscord-development <dylanlayton098@gmail.com>
2024-11-12 16:18:42 +0000

    Delete maintainance.js

--- pages/maintainance.js
+++
@@ -1,9 +0,0 @@
-// pages/maintenance.js
-export default function Maintenance() {
-    return (
-       <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
-          <h1 className="text-4xl font-bold">We’re currently undergoing maintenance. Please check back later!</h1>
-       </div>
-    );
- }
- 
