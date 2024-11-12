0dacc679c04783e772ac1f693cc407b2f916df4a
novadiscord-development <dylanlayton098@gmail.com>
2024-11-12 16:05:10 +0000

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,29 @@
-// pages/api/auth/[...nextauth].js
-import NextAuth from 'next-auth';
-import DiscordProvider from 'next-auth/providers/discord';
+import NextAuth from "next-auth/next";
+import DiscordProvider from "next-auth/providers/discord";
 
 export default NextAuth({
    providers: [
       DiscordProvider({
          clientId: process.env.DISCORD_CLIENT_ID,
          clientSecret: process.env.DISCORD_CLIENT_SECRET,
+         authorization: { params: { scope: "identify guilds" } },
       }),
    ],
-   secret: process.env.NEXTAUTH_SECRET,
+   callbacks: {
+      async jwt({ token, account }) {
+         if (account) {
+            token.accessToken = account.access_token;
+         }
+         return token;
+      },
+      async session({ session, token }) {
+         session.accessToken = token.accessToken;
+
+         if (session?.user) {
+            session.user.id = token.sub;
+         }
+
+         return session;
+      },
+   },
 });
